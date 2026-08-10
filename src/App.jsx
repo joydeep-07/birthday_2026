c2b7e7116ce934f67c945ba4342cc8c61e6032f8
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-08-10 18:33:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './layouts/Navbar'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
