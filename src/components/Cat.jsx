7016f06f21a9608c85db1b1a92be56dee7beff9f
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-08-21 14:14:47 +0530

    Updated UI

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import cat from "../assets/gadgets/cat.png";
-import cat2 from "../assets/gadgets/cat2.png";
 import cat3 from "../assets/gadgets/cat3.jpg";
 import cat4 from "../assets/gadgets/cat4.jpg";
 import cat5 from "../assets/gadgets/cat5.jpg";
@@ -12,17 +10,20 @@ const Cat = () => {
   const cats = [cat3, cat4, cat5, cat6, cat7, cat8];
 
   return (
-    <section className="w-full px-4 py-8 md:px-12">
-      <div className="main relative flex w-full items-center justify-center overflow-hidden border border-[#183B56]/10 bg-white/60 px-4 py-4 md:py-6 md:px-8">
-        <div className="relative z-10 flex w-full items-center justify-center gap-2 md:gap-6">
+    <section className="w-full px-3 py-8 md:px-12">
+      <div className="main relative flex w-full items-center justify-center overflow-hidden border border-[#183B56]/10 bg-white/60 px-4 py-4 md:py-8 md:px-8">
+        <div className="relative z-10 grid w-full grid-cols-2 items-end justify-center gap-2 md:flex md:gap-6">
           {cats.map((image, index) => (
-            <img
+            <div
               key={index}
-              src={image}
-              loading="lazy"
-              alt={`Cat ${index + 1}`}
-              className="h-36 w-1/4 object-contain md:h-52"
-            />
+              className="flex aspect-square w-full items-center justify-center md:aspect-auto md:w-auto"
+            >
+              <img
+                src={image}
+                alt={`Cat ${index + 1}`}
+                className="h-full w-full object-cover md:h-52 md:w-auto"
+              />
+            </div>
           ))}
         </div>
       </div>
